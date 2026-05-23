6c9d8b56e5f33964ed14ee6fb3fc2015ea398c76
VictorRavani <victorravani98@gmail.com>
2026-05-23 15:18:32 -0300

    Key

--- main.py
+++ main.py
@@ -1969,4 +1969,4 @@ def dados():
 #app.run(host='0.0.0.0', port=5000, debug=True)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
